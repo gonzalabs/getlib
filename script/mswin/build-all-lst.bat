28c8abb019f9be82297eea429d62c6bbe933bc2b
joker <anonimus@null.net>
2012-01-29 16:05:54 -0500

    librares list was extracted into a separate file build-all-lst.bat

---
+++ script/mswin/build-all-lst.bat
@@ -0,0 +1,19 @@
+::
+:: build libraries
+::
+	
+::call "%buildlib%" CFG library-name library-version [library-directory]
+:: CFG could be [dll, lib or all]
+	
+::call "%buildlib%" all bzip2 1.0.6
+call "%buildlib%" lib expat 2.0.1
+call "%buildlib%" lib freetype 2.4.8
+
+call "%buildlib%" lib zlib 1.2.5
+call "%buildlib%" lib libpng 1.5.7 lpng157
+call "%buildlib%" lib jpeg 8d
+
+call "%buildlib%" lib libogg 1.3.0
+call "%buildlib%" lib libvorbis 1.3.2
+call "%buildlib%" lib libtheora 1.1.1
+	
--- script/mswin/build-all.bat
+++ script/mswin/build-all.bat
@@ -194,20 +194,7 @@ echo ...........................................................................
 	set LIB=%outdir-lib%;%LIB%
 
 :: build libraries
-	::call "%buildlib%" CFG library-name library-version [library-directory]
-	:: CFG could be [dll, lib or all]
-	
-	::call "%buildlib%" all bzip2 1.0.6
-	call "%buildlib%" lib expat 2.0.1
-	call "%buildlib%" lib freetype 2.4.8
-	
-	call "%buildlib%" all zlib 1.2.5
-	call "%buildlib%" lib libpng 1.5.7 lpng157
-	call "%buildlib%" lib jpeg 8d
-	
-	call "%buildlib%" lib libogg 1.3.0
-	call "%buildlib%" lib libvorbis 1.3.2
-	call "%buildlib%" lib libtheora 1.1.1
+	call "%~dp0\build-all-lst.bat"
 	
 :: restore paths
 	set INCLUDE=%SAVE_INCLUDE%
